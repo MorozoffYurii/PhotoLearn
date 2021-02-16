cce9cb14a3d4d24c5efe3b02c277aa130fffd220
Yurii <yuramorozov2711@gmail.com>
2021-02-16 21:53:51 +0300

    After class changes

--- Assets/Screenshot.cs
+++ Assets/Screenshot.cs
@@ -6,24 +6,36 @@ using UnityEngine.UI;
 public class Screenshot : MonoBehaviour
 {
     private static Screenshot instance;
-    public Image image;
     Camera myCamera;
-    bool takeScreenshotOnNextFrame;
+    public GameObject Photo;
+    public float printTime;
+    bool printing;
+
 
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
         myCamera = GetComponent<Camera>();
+        printing = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (takeScreenshotOnNextFrame)
-        {
-            takeScreenshotOnNextFrame = false;
 
+    }
+
+    void NewPaper()
+    {
+
+    }
+
+    void TakeScreenshot(int width, int heigh)
+    {
+        if (!printing)
+        {
+            //Creating Screenshot
             RenderTexture.active = myCamera.targetTexture;
 
             Texture2D _tex2D = new Texture2D(RenderTexture.active.width, RenderTexture.active.height);
@@ -33,27 +45,24 @@ public class Screenshot : MonoBehaviour
             _tex2D.Apply(false);
 
             Sprite sprite = Sprite.Create(_tex2D, rect, Vector3.zero);
-            image.sprite = sprite;
 
-            /*RenderTexture renderTexture = myCamera.targetTexture;
-            Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
-            Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
-            renderResult.ReadPixels(rect, 0, 0);
-            renderResult.Apply();
-            Sprite sprite = Sprite.Create(renderResult, rect, Vector3.zero);
-            image.sprite = sprite;
-            */
-            //byte[] byteArray = renderResult.EncodeToPNG();
-            //System.IO.File.WriteAllBytes(Application.dataPath + "/CameraScreenhot.png", byteArray);
-            //RenderTexture.ReleaseTemporary(renderTexture);
-            //myCamera.targetTexture = null;
-        }
+            //image.sprite = sprite;
+
+            //Creating photo
+            GameObject photo = Instantiate(Photo, Photo.transform.position, Photo.transform.rotation);
+            photo.SetActive(true);
+            photo.GetComponent<PhotoInfo>().image.sprite = sprite;
+            photo.LeanMoveX(2.75f, printTime);
+            printing = true;
+            StartCoroutine(resumePrinting(photo.GetComponent<Rigidbody>()));
+        }        
     }
 
-    void TakeScreenshot(int width, int heigh)
+    IEnumerator resumePrinting(Rigidbody rigidbody)
     {
-        //myCamera.targetTexture = RenderTexture.GetTemporary(width, heigh, 16);
-        takeScreenshotOnNextFrame = true;
+        yield return new WaitForSeconds(printTime);
+        rigidbody.isKinematic = false;
+        printing = false;
     }
 
     public static void TakeScreenshot_Static(int width, int heigh)
