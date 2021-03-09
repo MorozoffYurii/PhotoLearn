a9fe9f997106f51eb38eb9a19b9120997fda1d5a
darke <sirotenko.max1998@gmail.com>
2021-03-09 22:12:11 +0300

    Poses Changer

---
+++ Assets/Scripts/ChangePoses.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePoses : MonoBehaviour
+{
+    [SerializeField] List<GameObject> Poses;
+    int i = 0;
+    // Start is called before the first frame update
+    
+    public void NextPos()
+    {
+        Poses[i].SetActive(false);
+        i++;
+        if (i == Poses.Count)
+            i = 0;
+        Poses[i].SetActive(true);
+    }
+
+    public void PreviousPos()
+    {
+        Poses[i].SetActive(false);
+        i--;
+        if (i == -1)
+            i = Poses.Count - 1;
+        Poses[i].SetActive(true);
+    }
+
+
+
+}
