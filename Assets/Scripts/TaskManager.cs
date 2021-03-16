385a22fa9d140b66dd26c4c9ba2d55d147fea294
darke <sirotenko.max1998@gmail.com>
2021-03-16 22:47:30 +0300

    Task manager, collider, correct photo recognition

---
+++ Assets/Scripts/TaskManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskManager : MonoBehaviour
+{
+    [Serializable]
+    public class Task
+    {
+        public Collider Target;
+        public string text;
+        public bool Horizontal;
+    }
+
+    [SerializeField] List<Task> tasks;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
